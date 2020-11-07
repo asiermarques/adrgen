41bae792b07ad136afc44085ab4eb78f439388ea
Asier Marqués <asiermarques@gmail.com>
2020-11-07 14:23:26 +0100

    readme update

--- README.md
+++ README.md
@@ -17,6 +17,27 @@ go build -o adrgen
 
 ### Create a new ADR
 
+**Simple Usage**
+
 ```
 adrgen create "My new adr"
 ```
+
+**Specify meta parameters**
+
+```
+adrgen create "My new adr" -m "components, technologies"
+```
+
+The meta parameters will add a meta section on the top of the generated file.  
+```
+---
+components: ""
+technologies: ""
+---
+
+# My new adr
+```
+
+They could be useful for an automated process that use the ADR files to generate living documentation.
+
--- cmd/create.go
+++ cmd/create.go
@@ -13,7 +13,7 @@ func NewCreateCmd() *cobra.Command {
 	command := &cobra.Command{
 		Use:   "create",
 		Short: "Create a new ADR File in the current directory",
-		Long: `Create a new ADR File in the current directory`,
+		Long: `Create a new ADR File in the current directory, you can add meta parameters for decisions tracing`,
 		Args: cobra.ExactArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
 			directory, err := os.Getwd()
@@ -30,6 +30,7 @@ func NewCreateCmd() *cobra.Command {
 			for i, value := range meta {
 				meta[i] = strings.TrimSpace(value)
 			}
+
 			_, creationError := application.CreateADRFile(args[0], directory, os.Getenv("ADRGEN_TEMPLATE"), meta)
 			if creationError!=nil {
 				fmt.Println(err)
@@ -42,14 +43,4 @@ func NewCreateCmd() *cobra.Command {
 
 func init() {
 	rootCmd.AddCommand(NewCreateCmd())
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// genCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// genCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
