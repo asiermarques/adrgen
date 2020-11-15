27fce1c9c65c8958783f1229f8dc74305981d8e8
Asier Marqués <asiermarques@gmail.com>
2020-11-15 15:00:39 +0100

    wip adr create e2e testing

---
+++ docs/features/features_definition_steps/common_test.go
@@ -0,0 +1,27 @@
+package features_definition_steps
+
+import (
+	"fmt"
+	"github.com/cucumber/godog"
+	"os/exec"
+)
+
+func SuiteContext(t *godog.TestSuiteContext)  {
+	t.BeforeSuite(func() {
+		output, err := exec.Command("/bin/sh", "-c", "cd ../../../; go build -o docs/features/e2e/bin/adrgen").CombinedOutput()
+		if err != nil {
+			fmt.Printf("error generating the adrgen binary: %s %s", err, output)
+		}
+		output, err = exec.Command("/bin/sh", "-c", "cd ../e2e; mkdir tests").CombinedOutput()
+		if err != nil {
+			fmt.Printf("error creating the tests directory: %s %s", err, output)
+		}
+	})
+
+	t.AfterSuite(func() {
+		output, err := exec.Command("/bin/sh", "-c", "rm -rf ../e2e").CombinedOutput()
+		if err != nil {
+			fmt.Printf("error removing test directory: %s %s", err, output)
+		}
+	})
+}
--- docs/features/features_definition_steps/create_file_steps_test.go
+++ docs/features/features_definition_steps/create_file_steps_test.go
@@ -1,44 +1,126 @@
 package features_definition_steps
 
 import (
+	"fmt"
 	"github.com/cucumber/godog"
 	"github.com/cucumber/messages-go/v10"
+	"os/exec"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
-func aNewadrmdIsCreated(arg1 int) error {
-	return godog.ErrPending
+var userTitle string
+var createdFilename string
+
+func aNewFileIsCreated(filename string) error {
+	output, err := exec.Command("/bin/sh", "-c", fmt.Sprintf("cd ../e2e/tests; ls \"%s\"",filename)).CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("file was not created: %s %s", err, output)
+	}
+
+	createdFilename = filename
+	return nil
 }
 
-func theAdrFileContentHasTheNewAdrTitle(arg1 int) error {
-	return godog.ErrPending
+func theAdrFileContentHasTheTitle(titleInContent string) error {
+	titleInContent = "# " + titleInContent
+	searchCommand := fmt.Sprintf(`grep -E "^# (.+)$" %s`, createdFilename)
+
+	output, err := exec.Command("/bin/sh", "-c", fmt.Sprintf("cd ../e2e/tests; %s", searchCommand)).CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("error searching string in file: %s %s", err, output)
+	}
+
+	returned := strings.TrimSpace(string(output))
+	if returned != titleInContent {
+		return fmt.Errorf("expected title: \"%s\"  found: \"%s\"", titleInContent, returned)
+	}
+
+	return nil
 }
 
-func theAdrHasAProposedStatus() error {
-	return godog.ErrPending
+func theAdrHasTheStatus(status string) error {
+	searchCommand := fmt.Sprintf(`grep -E "^Status: (.+)$" %s`, createdFilename)
+
+	output, err := exec.Command("/bin/sh", "-c", fmt.Sprintf("cd ../e2e/tests; %s", searchCommand)).CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("error searching string in file: %s %s", err, output)
+	}
+
+	returned := strings.TrimSpace(string(output))
+	expected := "Status: " + status
+	if returned != expected {
+		return fmt.Errorf("expected status: \"%s\"  found: \"%s\"", expected, returned)
+	}
+
+	return nil
 }
 
-func theAdrHasAnId(arg1 int) error {
-	return godog.ErrPending
+func theAdrHasAnId(adrId int) error {
+	re := regexp.MustCompile(`(?mi)^(\d+)-.+\.md`)
+	matches := re.FindStringSubmatch(createdFilename)
+	if len(matches) < 2 {
+		return fmt.Errorf("filename not valid %s", createdFilename)
+	}
+
+	matchId, _ := strconv.Atoi(matches[1])
+	if matchId != adrId {
+		return fmt.Errorf("expected status: \"%d\"  found: \"%d\"", adrId, matchId)
+	}
+
+	return nil
 }
 
 func theCommandIsExecuted() error {
-	return godog.ErrPending
+	output, err := exec.Command("/bin/sh", "-c", fmt.Sprintf("cd ../e2e/tests; ../bin/adrgen create \"%s\"",userTitle)).CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("error executing the create command: %s %s", err, output)
+	}
+
+	return nil
 }
 
-func theUserSpecifyTheNewAdrTitle() error {
-	return godog.ErrPending
+func theUserSpecifyTheTitle(title string) error {
+	userTitle = title
+	return nil
 }
 
-func thereIsAConfigFileCreatedWithThisConfiguration(arg1 *messages.PickleStepArgument_PickleTable) error {
-	return godog.ErrPending
+func thereIsAConfigFileCreatedWithThisConfiguration(table *messages.PickleStepArgument_PickleTable) error {
+	content := `default_meta: []
+default_status: proposed
+directory: docs
+id_digit_number: 4
+supported_statuses:
+	- proposed
+	- accepted
+	- rejected
+	- superseded
+	- amended
+	- deprecated
+template_file: docs/adr_template.md
+`
+	configFile := "adrgen.config.yml"
+
+	output, err := exec.Command("/bin/sh", "-c", fmt.Sprintf(
+		"cd ../e2e/tests; touch %s; echo \"%s\" > %s",
+				configFile,
+				content,
+				configFile,
+		)).CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("error generating the config file: %s %s", err, output)
+	}
+
+	return nil
 }
 
-func FeatureContext(s *godog.Suite) {
-	s.Step(`^a (\d+)-new-adr\.md is created$`, aNewadrmdIsCreated)
-	s.Step(`^the adr file content has the (\d+)\. New adr title$`, theAdrFileContentHasTheNewAdrTitle)
-	s.Step(`^the adr has a proposed status$`, theAdrHasAProposedStatus)
+func FeatureContext(s *godog.ScenarioContext) {
+	s.Step(`^a (.+) is created$`, aNewFileIsCreated)
+	s.Step(`^the adr file content has the (.+) title$`, theAdrFileContentHasTheTitle)
+	s.Step(`^the adr has a (.+) status$`, theAdrHasTheStatus)
 	s.Step(`^the adr has an id (\d+)$`, theAdrHasAnId)
 	s.Step(`^the command is executed$`, theCommandIsExecuted)
-	s.Step(`^the user specify the New adr title$`, theUserSpecifyTheNewAdrTitle)
+	s.Step(`^the user specify the (.+) title$`, theUserSpecifyTheTitle)
 	s.Step(`^there is a config file created with this configuration$`, thereIsAConfigFileCreatedWithThisConfiguration)
 }
